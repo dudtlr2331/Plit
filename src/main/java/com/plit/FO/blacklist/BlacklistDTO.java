98988a018fc6cb559ec5219cf7f6a0bb4dd860a9
syc <slahzns@gmail.com>
2025-06-28 17:31:20 +0900

    Revert "3번째 커밋"
    This reverts commit db1f1a13aaebc11fa5506e2102ddba9aa1087699.

--- src/main/java/com/plit/FO/blacklist/BlacklistDTO.java
+++ src/main/java/com/plit/FO/blacklist/BlacklistDTO.java
@@ -1,25 +1,4 @@
 package com.plit.FO.blacklist;
 
-import lombok.*;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class BlacklistDTO {
-    private Integer blackListNo;
-    private Integer reporterId;
-    private Integer reportedUserId;
-    private String reason;
-    private String status;
-    private Integer handledBy;
-    private String reportedAt;
-    private String handledAt;
-
-    // 신고자 닉네임
-    private String reporterNickname;
-    
-    // 신고당한 유저 닉네임
-    private String reportedNickname;
 }
--- src/main/java/com/plit/FO/blacklist/BlacklistRepository.java
+++ src/main/java/com/plit/FO/blacklist/BlacklistRepository.java
@@ -1,10 +1,4 @@
 package com.plit.FO.blacklist;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BlacklistRepository extends JpaRepository<BlacklistEntity, Integer> {
-    // 추후 사용자별 신고 내역 조회용 메서드도 추가 가능
-
+public class BlacklistRepository {
 }
