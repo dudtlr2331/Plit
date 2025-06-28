db1f1a13aaebc11fa5506e2102ddba9aa1087699
syc <slahzns@gmail.com>
2025-06-28 17:31:04 +0900

    3번째 커밋 비매너 유저 차단 페이지 기본 클래스 틀 마련 로그인 되어있는 상태에서의 기능 구현 result.html 을 통한 페이지 출력

--- src/main/java/com/plit/FO/blacklist/BlacklistRepository.java
+++ src/main/java/com/plit/FO/blacklist/BlacklistRepository.java
@@ -1,4 +1,10 @@
 package com.plit.FO.blacklist;
 
-public class BlacklistRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlacklistRepository extends JpaRepository<BlacklistEntity, Integer> {
+    // 추후 사용자별 신고 내역 조회용 메서드도 추가 가능
+
 }
