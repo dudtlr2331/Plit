ced2a9b75f00e2a8350ca2f86ec8492de6aa3705
syc <slahzns@gmail.com>
2025-06-28 17:36:52 +0900

    3번째 커밋 비매너유저 신고 관련 기본 클래스 구현 report.html 로 설정한 신고 페이지 구현 로그인 되어있는 계정의 정보를 유지하도록 설정 신고하기를 눌렀을 때 자동으로 로그인된 계정의 닉네임을 불러오도록 설정

--- src/main/java/com/plit/FO/blacklist/BlacklistController.java
+++ src/main/java/com/plit/FO/blacklist/BlacklistController.java
@@ -1,4 +1,19 @@
 package com.plit.FO.blacklist;
 
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class BlacklistController {
+
+    @GetMapping("/report")
+    public String getBlacklist(Model model, HttpSession session) {
+        Object loginUser = session.getAttribute("loginUser");
+        if (loginUser != null) {
+            model.addAttribute("loginUser", loginUser);
+        }
+        return "fo/blacklist/report";
+    }
 }
