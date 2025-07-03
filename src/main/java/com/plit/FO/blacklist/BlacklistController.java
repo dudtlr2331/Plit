b434723ea08289a3a844e07d54eaf001b3e3b521
syc <slahzns@gmail.com>
2025-07-03 13:17:30 +0900

    12번째 커밋 [UPDATE] - 트롤 신고 게시판: DB 기반 정보 출력 기능 구현
    [ADD]
    - 다른 사람이 이미 신고한 유저도 신고할 수 있는 버튼 기능 추가
    - 신고 등록 시간 경과 표시 기능 (초/분/일 단위) 추가
    
    [REMOVE]
    - 관리자 계정 생성 기능 제거
    - 관련 JS 및 Controller 메서드 삭제

--- src/main/java/com/plit/FO/blacklist/BlacklistController.java
+++ src/main/java/com/plit/FO/blacklist/BlacklistController.java
@@ -1,12 +1,18 @@
 package com.plit.FO.blacklist;
 
+import com.plit.FO.user.UserDTO;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class BlacklistController {
+    @Autowired
+    private BlacklistService blacklistService;
 
     @GetMapping("/report")
     public String getBlacklist(Model model, HttpSession session) {
@@ -14,6 +20,11 @@ public class BlacklistController {
         if (loginUser != null) {
             model.addAttribute("loginUser", loginUser);
         }
+
+        Integer currentUserSeq = (loginUser != null) ? ((UserDTO) loginUser).getUserSeq() : -1;
+        List<BlacklistDTO> blacklist = blacklistService.getAllReportsWithCount(currentUserSeq);
+        model.addAttribute("blacklist", blacklist);
+
         return "fo/blacklist/report";
     }
 }
